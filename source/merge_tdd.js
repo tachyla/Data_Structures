d3f66354cc7f60f6f77d51e8553fbbdbe6b0277e
TaChyla <tachyla.murray@leadingagile.com>
2022-09-12 14:05:31 -0400

    failing test refactor while loops to for loop

--- source/merge_tdd.js
+++ source/merge_tdd.js
@@ -1,56 +1,52 @@
-//breaks down arr until arr.len === 1
 const mergeSort = (array) => {
     if(array.length === 1) return array;
 
-    //logically inserts elements in numeric order
-    const mergeHelper = (arrayA, arrayB) => {
-        let resultArray = [];
-        
-        // while(i1 < arrayA.length && i2 < arrayB.length){
-        //     //if arrayA[index1] is smallest; push  arrayA[index1]; increase index pointer
-        //     if(arrayA[i1] < arrayB[i2]){
-        //         resultArray.push(arrayA[i1]);
-        //         i1++;
-        //     }
-        //     //otherwise push arrayB[index2]; increase pointer index
-        //     else{
-        //         resultArray.push(arrayB[i2]);
-        //         i2++;
-        //     }
-        // }
-        if(arrayA.length && arrayB.length){
-            for(let indexA = 0; indexA < arrayA.length; indexA++){
-                if(arrayA[indexA] < arrayB[indexB]){
-                    resultArray.push(arrayA[indexA]);
-                    indexA++;
-                }
-            }
-
-            for(let indexB = 0; indexB < arrayB.length; indexB++){
-                if(arrayA[indexA] > arrayB[indexB]){
-                    resultArray.push(arrayB[indexB]);
-                    indexB++;
-                }
-                else{ resultArray.push(arrayA[indexA]);
-                indexA++
-            };
-            }
-        }
-        else if(indexA < arrayA.length){
-            resultArray.push(arrayA[indexA]);
-            indexA++;
-        }
-
-        else{}
-        
-        return resultArray;
-    }
-    
-    //recursively break down array length that is not 1
     while(array.length > 1){
         let middle = Math.floor(array.length / 2);
         let right = array.slice(middle);  
         let left = array.slice(0, middle);
+
+        const mergeHelper = (arrayA, arrayB) => {
+            let resultArray = [];
+
+            let i1 = 0;
+            let i2 = 0;
+            
+            //if more than one array has length
+            if(i1 < arrayA.length && i2 < arrayB.length){
+
+                if(arrayA[i1] < arrayB[i2]){
+                    resultArray.push(arrayA[i1]);
+                    i1++;
+                }
+
+                if(arrayA[i1] > arrayB[i2]){
+                    resultArray.push(arrayA[i1]);
+                    i1++;
+                }
+                return resultArray;
+            }
+
+            //one array has length
+            if(i1 < arrayA.length || i2 < arrayB.length){
+
+                if(i1 < arrayA.length) {
+                    for(i1 < arrayA.length; i1++;){
+                        resultArray.push(arrayA[i1]);
+                    }
+                    return resultArray;
+                }
+
+                if(i2 < arrayB.length){
+                    for(i2 < arrayB.length; i2++;){
+                        resultArray.push(arrayB[i2]);
+                    }
+                    return resultArray;
+                }
+            }
+            
+            return resultArray;
+        }
 
         //call mergeHelper on each array portion 
         return mergeHelper(mergeSort(left), mergeSort(right));
