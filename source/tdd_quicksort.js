64a1e7e0eb9ef6606c388929ebfeaa8e510468cf
TaChyla <tachyla.murray@leadingagile.com>
2022-09-13 06:57:52 -0400

    returns array if length != 1

--- source/tdd_quicksort.js
+++ source/tdd_quicksort.js
@@ -1,26 +1,13 @@
 const quickSort = (array) => {
     if(array.length === 1) return array;
 
-    let swapFunction = (array, index1, index2) => {
-        let temp = array[index1];
-        array[index1] = array[index2];
-        array[index2] = temp;
-    }
-
-    if(array.length > 1){
-        let pivotIndex = 0;
-        swapIndex = pivotIndex;
-
-        for(let i = pivotIndex + 1; i < array.length; i++){
-            if(array[i] > array[pivotIndex]){
-                //swap indicies
-                swap++;
-                swap(array, swapIndex, i);
-            }
-        }
-    }
-    console.log(array);
-    return array;
+    else return array;
 }
+
+
+
+
+
+
 
 module.exports = quickSort;
